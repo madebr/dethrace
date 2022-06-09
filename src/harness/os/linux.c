df040802260b934c3acc023ee0408d9c4052ea12
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-06-09 23:24:27 +0200

    fix macos try #1

--- src/harness/os/linux.c
+++ src/harness/os/linux.c
@@ -2,23 +2,19 @@
 
 #include "harness/os.h"
 
-#include <assert.h>
 #include <ctype.h>
 #include <dirent.h>
 #include <err.h>
-#include <errno.h>
 #include <execinfo.h>
 #include <fcntl.h>
 #include <libgen.h>
 #include <limits.h>
 #include <signal.h>
-#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
-#include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
 
