2f130b6bb27dd168bd00cb36d2f6354a4fd48800
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-06-19 22:02:19 +0200

    android WIP

---
+++ ports/android/gradle/app/src/main/java/dethrace_labs/dethrace/MainActivity.java
@@ -0,0 +1,9 @@
+package dethrace_labs.dethrace;
+
+import org.libsdl.app.SDLActivity;
+
+public class MyActivity extends SDLActivity {
+    protected String[] getLibraries() {
+        return new String[] {"dethrace"};
+    }
+}
